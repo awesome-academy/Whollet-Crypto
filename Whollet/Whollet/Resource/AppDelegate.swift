27223adaaf22b0aae1c87d97576290f7ec226013
tuta-3205 <tuta@tutas-MacBook-Pro.local>
2022-11-14 15:42:14 +0700

    feat: Launch Screen

--- Whollet/Whollet/Resource/AppDelegate.swift
+++ Whollet/Whollet/Resource/AppDelegate.swift
@@ -7,6 +7,13 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(
+            name: "OnboardingScreen",
+            bundle: nil
+        ).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+
         return true
     }
 }
