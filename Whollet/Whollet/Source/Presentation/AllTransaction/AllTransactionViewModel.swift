c362aaf49f0a087da28adfb731544beceee4ac4b
tuta-3205c <tran.anh.tu-b@sun-asterisk.com>
2022-11-28 11:07:03 +0700

    feat: comfirm

---
+++ Whollet/Whollet/Source/Presentation/AllTransaction/AllTransactionViewModel.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+final class AllTransactionViewModel: NSObject {
+    private var getAllTransactionHistoryUseCase: GetAllTransactionHistoryUseCase!
+    
+    private(set) var bsTransactions: BehaviorSubject<[TransactionDetail]> = BehaviorSubject(nil)
+    
+    override init() {
+        super.init()
+        getAllTransactionHistoryUseCase = GetAllTransactionHistoryUseCase()
+        getTransactions()
+    }
+    
+    private func getTransactions() {
+        getAllTransactionHistoryUseCase.get { [weak self] (data, error) -> (Void) in
+            guard let self = self, error == nil, let data = data else { return }
+            self.bsTransactions.add(data)
+        }
+    }
+}
